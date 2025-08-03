b1e5744d2ea22a7e42b22275993f5964a178aa07
Simon Walton <simonwalton@Simons-Air.communityfibre.co.uk>
2025-08-03 20:18:20 +0100

    ensure each bar absolutely positioned using translateY and useRef to keep element in the dom

--- app/_components/BarChart/index.tsx
+++ app/_components/BarChart/index.tsx
@@ -2,6 +2,7 @@
 
 import { iPopulationByCountry } from '@/lib/data/dataTypes';
 import { iColourMap } from '@/app/page';
+import { useEffect, useRef } from 'react';
 
 interface BarChartProps {
   data: Array<iPopulationByCountry>;
@@ -11,32 +12,52 @@ interface BarChartProps {
 export function BarChart({ data, colours }: BarChartProps) {
   const maxPopulation = Math.max(...data.map((d) => d.Population));
   const sorted = [...data].sort((a, b) => b.Population - a.Population);
-  // Ensure max of 15 countries shown
   const top15Countries = sorted.slice(0, 15);
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+    const nodes = containerRef.current.querySelectorAll('[data-country]');
+    const newPosition: Record<string, number> = {};
+
+    nodes.forEach((node) => {
+      const domRect = (node as HTMLElement).getBoundingClientRect();
+      const country = (node as HTMLElement).dataset.country!;
+      newPosition[country] = domRect.top;
+    });
+  }, [data]);
+
   return (
-    <div className="flex flex-col gap-2 w-full mt-6 transition-all duration-700">
-      {top15Countries.map((country) => {
-        const widthPercent = (country.Population / maxPopulation) * 100;
-        const colour = colours[country.Country] || "#999";
+    <div className="relative h-[680px]" ref={containerRef}>
+      {top15Countries.map((dataPoint, index) => {
+        const barHeight = 45;
+        const yTranslate = index * barHeight;
+        const colour = colours[dataPoint.Country] || '#000';
+        const widthPercent = (dataPoint.Population / maxPopulation) * 100;
 
         return (
           <div
-            key={country.Country}
-            className="relative flex items-center gap-4 transition-all duration-700 ease-in-out"
+            key={dataPoint.Country}
+            data-country={dataPoint.Country}
+            className="absolute left-0 w-full flex items-center gap-4 transition-transform duration-800"
+            style={{
+              transform: `translateY(${yTranslate}px)`,
+              height: `${barHeight - 6}px`,
+            }}
           >
-            <span className="w-32 truncate text-sm">{country.Country}</span>
-            <div className=" w-full h-6 bg-gray-100 rounded overflow-hidden">
+            <span className="w-32 truncate text-sm">{dataPoint.Country}</span>
+            <div className="w-full h-full overflow-hidden">
               <div
-                className="h-6 rounded"
+                className="h-full rounded transition-all duration-500 ease-in-out"
                 style={{
                   width: `${widthPercent}%`,
                   backgroundColor: colour,
                 }}
               />
             </div>
-            <span className="w-32  text-xs h-6 flex items-center text-gray-700">
-              {country.Population.toLocaleString()}
+            <span className="w-32 text-xs h-full flex items-center text-gray-700">
+              {dataPoint.Population.toLocaleString()}
             </span>
           </div>
         );
